d20e3c3a9292b81fa59b70268952107a8d0f973c
Stephen Beckstrom-Sternberg <Stephen.Beckstrom-Sternberg@azdhs.gov>
2024-04-18 09:01:57 -0700

    Update Freyja_summarizer-v0.8.3.py
    Signed-off-by: Stephen Beckstrom-Sternberg <Stephen.Beckstrom-Sternberg@azdhs.gov>

--- Freyja_summarizer-v0.8.3.py
+++ Freyja_summarizer-v0.8.3.py
@@ -22,8 +22,6 @@ outputFile = "Freyja_aggregated_summary.tsv"
 def abundance_simplify(abundance):
     abundance = re.sub(r"1\.00(\d)+\D*", r"1.00", abundance)
     abundance = re.sub(r"0\.(\d)(\d)(\d)(\d)(\d)+\D*", r".\1\2\3\4", abundance)
-#    abundance = re.sub(r"0(\d)\.(\d)(\d)(\d)+", r"\1.\2\3%", abundance)
-#    abundance = re.sub(r'\)\]', r"", abundance)
     return abundance
 
 """Get simplified lineage names"""
@@ -70,7 +68,6 @@ def process_aggregated_freya_file(aggregated_file):
 
         """Parse summarized data"""
         lin_abundances = summarized.split("), (")
-#        print("Number of lin_abundances: ", len(lin_abundances))
 
         """Separate lineage and abundance from within lin_abundances for simplifying"""
         new_lin_abundances = []
@@ -78,7 +75,6 @@ def process_aggregated_freya_file(aggregated_file):
             (lineage, abundance) = i.split(", ")  # get tuple from each i
             lineage = perform_name_replacements(lineage)
             abundance = abundance_simplify(abundance)
-
             linHash = {lineage: abundance}
             
             """Append linHash to new_lin_abundances, rather than replacing it"""
@@ -93,33 +89,17 @@ def process_aggregated_freya_file(aggregated_file):
 
         """Create new_sH row and add to strainHash"""
         new_sH = {sample_covg:new_lin_abundances}
-    
-#        print("new_sH: " +str(new_sH))
         strainHash.update(new_sH)
-
-#    return (strainHash, sortedLineages)
     return (strainHash, sortedLineages)
-
 
 def save_df(strainHash,sortedLineages):
     new_strainHash = {}
-    ##NEW STUFF
-    
-    
-    lineageHeader = ["Lineages" for _ in range(len(sortedLineages))]
-    print("LINEAGEHEADER: " +str(lineageHeader))
-#    lineageHeader = ["Lineages"]*len(sortedLineages)
-#    lineageTuple = list(zip(lineageHeader, sortedLineages))
-
-#    headerList = ["Sample","Coverage (%)"] + lineageTuple
     headerList = ["Sample","Coverage (%)"] + sortedLineages
     print("Sorted lineage list: ", sortedLineages)
     print("Number of lineages: " +str(len(sortedLineages)))
-    print("\ndataframe headerList: " +str(headerList))
-    
+   
     """Create empty dataframe with column headers"""
     df  = pd.DataFrame(columns = headerList)
-#    print("\ndf after creating empty dataframe with named column headers:\n", df)
     """Populate dataframe with rows from strainHash"""
     for sample_covg, lin_abundances in strainHash.items():
         sample, covg = sample_covg  # Unpack the tuple
@@ -131,26 +111,23 @@ def save_df(strainHash,sortedLineages):
         df = df.append(thisRow, ignore_index=True)  # add row to DataFrame
     df.replace(np.nan, 0, inplace=True)
     df.sort_values(by=["Sample"], inplace=True)
-#    print("\ndf after appending data rows to the DataFrame:\n", df)
     return(new_strainHash, df)
 
 if __name__ == "__main__":
 
     """Process aggregated Freyja file"""
     (strainHash,sortedLineages) = process_aggregated_freya_file(aggregated_file)
-    
-#    print("\n\nstrainHash after processing: " +str(strainHash))
     print("\n\nsortedLineages: " +str(sortedLineages))
     
     """Process strainHash - create new version of StrainHash new_strainHash, 
     with the header row containing sample, coverage, and all lineageKeys, and each data row 
     containing the abundance for each lineage, including null values"""
-
-    """Create header, prepending Sample and Coverage to the sorted lineages"""
-    
     (new_strainHash, df) = save_df(strainHash, sortedLineages)
+
+    """Print dataframe to screen"""
+    print("Dataframe to string: \n", df.to_string())
     
-    """Write out the modified dataframe as tsv"""
+    """Write modified dataframe as tsv"""
     open(outputFile,'w')
     df.to_csv(outputFile, sep="\t", index=False)
     print('Results written to ', outputFile)
